afccef0ffe2e9fc45f62aa0cde30748fc922f9f1
Harsh Siddhapura <harshsiddhapura200@gmail.com>
2023-12-29 02:01:40 -0700

    Code added

---
+++ Sem-1/IFT510 - Architecture /Class-Assignments/outOfMemory/outOfMemory.py
@@ -0,0 +1,32 @@
+import os
+import datetime
+import getpass
+
+def print_system_info():
+    # Get user data
+    os.system('clear') # os.system('clear') for Linux
+    username = getpass.getuser()
+    # Get computer information
+    computer_info = os.name
+    # Get current date and time
+    current_time = datetime.datetime.now()
+    # Format log message
+    log_message = f"User: {username}\nTime:{current_time}\nComputer Info: {computer_info}"
+    # Print log message
+    print(log_message)
+
+# Call the function to print the log
+print_system_info()
+
+import sys
+
+def simulate_out_of_memory():
+    while True:
+        # try:
+            # Allocate a large amount of memory
+        memory = [0] * sys.maxsize
+        # except MemoryError:
+        #     print("Out of memory")
+        #     break
+
+simulate_out_of_memory()
